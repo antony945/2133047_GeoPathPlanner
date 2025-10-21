15b8e1ce6704fb91dfa0043482e64aebd91e2aa7
Antonio Franzoso <antonio.franzoso@outlook.it>
2025-10-21 02:11:11 +0200

    Added comments about function of kafka classes.

--- source/routing/cmd/api/main.go
+++ source/routing/cmd/api/main.go
@@ -6,4 +6,8 @@ import (
 
 func main() {
 	fmt.Println("ROUTING MICROSERVICE")
+
+	// TODO: Run the KAFKA consumer for it to wait for upcoming messages
+
+	// TODO: Send the output produced by RoutingService as a new msg on another KAFKA topic, acting as producer
 }
--- source/routing/internal/kafka/producer.go
+++ source/routing/internal/kafka/producer.go
@@ -1,1 +1,4 @@
 package kafka
+
+// Given the output of the RoutingService, act as a producer by sending it onto a kafka topic so for the backend to read it.
+// Do it in a coordinate way as the consumer (maybe same number of workers in working pool?)
